5e773f55abc32581f5855c1afc0949193e50a753
rockboynton <rock.boynton@yahoo.com>
2018-12-11 01:23:06 -0600

    led_allOn() and led_allOff() done

--- led.c
+++ led.c
@@ -10,8 +10,6 @@
 #include <inttypes.h>
 #include <stdio.h>
 
-#include "stm32f446xx.h"
-
 //One solution for the current speed to have a local static variable in this file;
 
 
@@ -19,6 +17,7 @@ void led_init(){
 	// Enable the GPIOB in RCC_AHB1ENR
 	*(RCC_AHB1ENR) |= (1 << GPIOBEN);
 	// Turn on to set LED0 - LED9 to output mode ("01")
+	// Pins 5-10, 12-15
 	uint32_t ledAndMask = 0x00C003FF;
 	uint32_t ledOrMask = 0x55155400;
 	*(GPIOB_MODER) &= ledAndMask;
@@ -27,18 +26,21 @@ void led_init(){
 
 void led_allOn(){
 	//Insert code here (Hint use ODR)
-	//protect other pins
+	//protect other pins 
+	// BSRR sets set bits in upper half-word
+	*(GPIOB_BSRR) |= 0xF7E00000;
 }
 
 void led_allOff(){
 	//Insert code here (Hint use ODR)
 	//protect other pins
-
+	// BSRR resets set bits in lower half-word
+	*(GPIOB_BSRR) |= 0x0000F7E0;
 }
 
 void led_on(uint8_t ledIndex){
 	//Insert code here (Hint use BSRR may be helpful)
-
+	
 }
 
 void led_off(uint8_t ledIndex){
