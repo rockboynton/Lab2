cbce52de4e888ad60f89a636a028ed0be55e0ffd
rockboynton <rock.boynton@yahoo.com>
2018-12-13 00:11:14 -0600

    cleaned up delay

--- delay.c
+++ delay.c
@@ -13,20 +13,12 @@ void delay_1ms(uint32_t n){
 		// 16 MHz clock
 		*(STK_LOAD) = 16000;
  		// set 1 to enable and clock source
-		*(STK_CTRL) = (1 << EN) | (1 << CLKSOURCE); // TODO make sure this isn't |=
+		*(STK_CTRL) = (1 << EN) | (1 << CLKSOURCE); 
 
 		// Wait for countflag to be set
 		 uint32_t countFlag = 0;
 		 while (countFlag == 0) {
-		 	countFlag = *(STK_CTRL) & (1 << COUNTFLAG); // ? Does this work? it should.
+		 	countFlag = *(STK_CTRL) & (1 << COUNTFLAG); 
 		 }
-//		int done = 0;
-//		uint32_t countFlag = 0;
-//		while (done == 0) {
-//			countFlag = *(STK_CTRL) & (1 << COUNTFLAG);
-//			if (countFlag == (1 << COUNTFLAG)) {
-//				done = 1;
-//			}
-//		}
 	}
 }
